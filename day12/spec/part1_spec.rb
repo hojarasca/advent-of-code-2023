3b6673131c51ea3aea87dad6ef9120fe8dc975b9
juanFdS <juan9794@gmail.com>
2023-12-30 18:12:25 -0300

    Setup con parseo

--- day12/spec/part1_spec.rb
+++ day12/spec/part1_spec.rb
@@ -1,14 +1,27 @@
 require_relative './data'
-data = DATA
-# data = <<~DATA
-
-# DATA
+# data = DATA
+data = <<~DATA
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+DATA
 
 
 
 describe 'PART 1' do
-
   it "a" do
-    expect(2).to eq(3)
+    lines = data.split("\n")
+
+    lines.each do |line|
+      spring_values, groups = line.split(" ")
+      groups = groups.split(",").map(&:to_i)
+      puts "Spring values: #{spring_values}"
+      puts groups.inspect
+    end
+
+    expect(lines).to eq 2
   end
 end
